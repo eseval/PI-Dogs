293e86eb8c814bfbcc6194a587a5f31f95e7a28a
eseval <90922661+eseval@users.noreply.github.com>
2022-06-20 11:23:28 -0500

    changes backend

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,36 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      efaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.INTEGER,
+    },
+    image : {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
+    });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const DogRoute = require('./dogs');
+const TemperamentRoute = require('./temperaments');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dogs', DogRoute);
+router.use('/temperaments', TemperamentRoute);
 
 module.exports = router;
