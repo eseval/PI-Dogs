12b1ef2fb8c515ffe42519dbf5dae3c8aa743b3d
eseval <90922661+eseval@users.noreply.github.com>
2022-06-23 22:20:58 -0500

    first changes front

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,3 +14,18 @@ export function getDogs() {
         }
     }
 }
+
+export function getTemperaments() {
+    return async function (dispatch) {
+        try {
+            let json = await axios.get('localhost:3001/temperaments');
+            return dispatch({
+                type: 'GET_TEMPERAMENTS',
+                payload: json.data
+            })
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+}
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,6 +1,23 @@
-export default function rootReducer(state, action) {
-    return {
-        ...state,
-        ...action.payload
-};
+const initialState = {
+    dogs: [],
+    temperaments: [],
+    allDogs: [],
+}
+
+export default function rootReducer(state=initialState, action) {
+    switch (action.type) {
+        case 'GET_DOGS':
+            return {
+                ...state,
+                dogs: action.payload,
+                allDogs: action.payload
+            }
+        case 'GET_TEMPERAMENTS':
+            return {
+                ...state,
+                temperaments: action.payload
+            }
+        default:
+            return state
+    }
 }
