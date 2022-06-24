61c402ef2979d8b98749d835212413997c450dfa
eseval <90922661+eseval@users.noreply.github.com>
2022-06-23 21:45:55 -0500

    folders created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+import Home from './components/Home/Home';
+import Detail from './components/Detail/Detail';
+import Form from './components/Form/Form';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage}/>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/dogs/:id" component={Detail}/>
+          <Route exact path="/dog" component={Form}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export function getDogs() {
+    return async function (dispatch) {
+        try {
+            let json = await axios.get('localhost:3001/dogs');
+            return dispatch({
+                type: 'GET_DOGS',
+                payload: json.data
+            })
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+}
