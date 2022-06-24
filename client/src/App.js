8521a1851b4403e96ead5c1a8a5927b210faaa40
eseval <90922661+eseval@users.noreply.github.com>
2022-06-24 11:14:32 -0500

    reverted

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LandingPage from './components/LandingPage/LandingPage';
-import Home from './components/Home/Home';
-import Detail from './components/Detail/Detail';
-import Form from './components/Form/Form';
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage}/>
-          <Route exact path="/home" component={Home}/>
-          <Route exact path="/dogs/:id" component={Detail}/>
-          <Route exact path="/dog" component={Form}/>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <h1>Henry Dogs</h1>
+    </div>
   );
 }
 
